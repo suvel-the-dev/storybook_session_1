a1b98519afc59685242ec14fe397bb0ac0bd2261
Suvel <suvelrathneswar08@gmail.com>
2021-06-18 11:42:20 +0530

    Added Getting Started with storybook page

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,8 @@
 module.exports = {
   stories: [
     '../src/components/**/*.stories.js',
-    '../src/templates/**/*.stories.js'
+    '../src/templates/**/*.stories.js',
+    '../src/*.stories.mdx'
   ],
   "addons": [
     "@storybook/addon-links",
